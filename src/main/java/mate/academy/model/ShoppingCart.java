660c79f83cc9df9a3fa73447388303ed47649979
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-11-10 17:22:16 +0200

    ShoppingCart.java

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -14,10 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "shopping_cart")
-public class ShoppingCart {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     @OneToMany(
             mappedBy = "shoppingCart",
